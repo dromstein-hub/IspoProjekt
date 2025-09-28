23498bb410be62de865b20ac6044ad9b194d7b85
dromstein-hub <dromstein@gmail.com>
2025-09-29 00:44:24 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
+from .models import User
+
+@login.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 db = SQLAlchemy()
 migrate = Migrate()
