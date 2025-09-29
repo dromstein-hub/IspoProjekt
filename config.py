2959bdf07e54f973cdecfd3174a77c13744a6958
dromstein <dromstein@gmail.com>
2025-09-29 20:04:16 +0200

    Konflikte gelöst: lokale Version behalten

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://backuser:backpass@localhost/backdb')
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "supersecretkey"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET = os.environ.get('JWT_SECRET', 'change-me')
